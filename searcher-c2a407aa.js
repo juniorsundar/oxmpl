8cf7fbb72ac02ccc93d716b76e6b68fb73d5756e
juniorsundar <juniorsundar@users.noreply.github.com>
2026-01-04 18:25:12 +0000

    deploy: e111cc674a2e9da9af7298fb54e204b7fe2a9d5b

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-42d4528e.js',
+                path_to_root + 'searchindex-fdf3f3cd.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
